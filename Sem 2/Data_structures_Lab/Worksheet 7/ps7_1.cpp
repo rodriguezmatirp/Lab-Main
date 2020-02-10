dc6af09a781e68d1bb9949f9d1fe788aa0ad28f3
Tarun Pritam R <52533898+rodriguezmatirp@users.noreply.github.com>
2020-02-11 00:22:19 +0530

    commit

--- Sem 2/Data_structures_Lab/Worksheet 7/ps7_1.cpp
+++ Sem 2/Data_structures_Lab/Worksheet 7/ps7_1.cpp
@@ -60,7 +60,7 @@ bool Linkedlist::insert(int element, int search_)
         return false;
     else
     {
-        while (t->data_!= search_)
+        while (t->data_ != search_)
             t = t->link_;
         n->link_ = t->link_;
         t->link_ = n;
@@ -97,12 +97,12 @@ bool Linkedlist::delete_(bool choice)
         {
             head_ = t->link_;
             delete t;
-            return true ;
+            return true;
         }
         else if (choice == 1)
         {
-            while(t->link_!=NULL)
-                t=t->link_;
+            while (t->link_ != NULL)
+                t = t->link_;
             delete t->link_;
             t->link_ = NULL;
             return true;
@@ -113,28 +113,71 @@ bool Linkedlist::delete_(bool choice)
 
 void Linkedlist::display()
 {
-    if(head_ == NULL)
-        cout<<"NO DATA";
+    if (head_ == NULL)
+        cout << "NO DATA";
     else
     {
-        Node* t = head_;
-        while(t->link_ != NULL)
+        Node *t = head_;
+        while (t->link_ != NULL)
         {
-            cout<<t->data_<<endl;
+            cout << t->data_ << endl;
             t = t->link_;
         }
-        cout<<t->data_<<endl;
+        cout << t->data_ << endl;
         delete t;
     }
 }
 
 int main()
 {
-    int choice;
-    cout<<"1) Insert a new node at beggining of the list\n2) Insert a new node after a node\n3) Insert a new node at the end of the list\n4) Delete the first node of the list\n5) Delete an existing node after a node with element to be given\n6) Delete the last node of the list\n7) Display the elements of the list\n\n";
-    cout<<"Enter a valid choice";
-    switch(choice)
+    int choice, element, search;
+line:
+    cout << "1) Insert a new node at beggining of the list\n2) Insert a new node after a node\n3) Insert a new node at the end of the list\n4) Delete the first node of the list\n5) Delete an existing node after a node with element to be given\n6) Delete the last node of the list\n7) Display the elements of the list\n\n";
+    cout << "Enter a valid choice";
+    Linkedlist Nodes;
+    switch (choice)
     {
-        case 1:
+    case 1:
+        cout << "Enter the data of the node\n";
+        cin >> element;
+        if (Nodes.insert(element, false))
+            cout << "Operation Successful!!\n";
+        goto line;
+    case 2:
+        cout << "Enter the data of the node\n";
+        cin >> element;
+        cout << "Data of the Node to be searched\n";
+        cin >> search;
+        if (Nodes.insert(element, search))
+            cout << "Operation Successful!!\n";
+        goto line;
+    case 3:
+        cout << "Enter the data of the node\n";
+        cin >> element;
+        if (Nodes.insert(element, true))
+            cout << "Operation successful!!\n";
+        goto line;
+    case 4:
+        if (Nodes.delete_(false))
+            cout << "Operation Successful!!\n";
+        goto line;
+    case 5:
+        cout << "Enter the data of the node\n";
+        cin >> search;
+        if (Nodes.delete_(search))
+            cout << "Operation Successful!!\n";
+        goto line;
+    case 6:
+        if (Nodes.delete_(true))
+            cout << "Operation Successful!!\n";
+        goto line;
+    case 7:
+        Nodes.display();
+        goto line;
+    case 8:
+        exit(0);
+    default:
+        cout << "Enter valid choice\n";
+        goto line;
     }
 }
