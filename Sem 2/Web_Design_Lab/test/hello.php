9242c754039c4d735b44ea9d5f2988e9044c6c6f
Tarun Pritam R <52533898+rodriguezmatirp@users.noreply.github.com>
2020-03-12 11:41:28 +0530

    commit

---
+++ Sem 2/Web_Design_Lab/test/hello.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+ <?php
+// $x = "abc";
+// $$x = 50;
+// echo "My first PHP script!";
+// echo "$x<br>";
+// echo $$x;
+// var_dump($x);
+// var_dump($$x);
+// echo "<br>";
+$cars = array("volvo,bmw,benz");
+echo $cars[0]."arrcar 1";
+// var_dump($cars); 
+
+$x = "hello world";
+$x = NULL;
+var_dump($x);
+?>
+
+</body>
+</html
